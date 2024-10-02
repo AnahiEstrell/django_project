59baba936758ee84a2bbbe39d9a4b89566b5095e
Anahi-Estrella <oestrella500@gmail.com>
2024-10-02 16:05:02 -0600

    Adding update and get for the authenticated user

--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets, permissions
-from django.contrib.auth import get_user_model
+from rest_framework import (
+    viewsets,
+    permissions,
+    generics,
+)
 from drf_spectacular.utils import extend_schema
 
 from user.serializers import UserSerializer
 from user.models import User
+
 
 @extend_schema(tags=['User'])
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,3 +16,15 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_class = [permissions.IsAuthenticated]
+
+
+@extend_schema(tags=['Me'])
+class UserMeView(generics.RetrieveUpdateAPIView):
+    """ View for authenticated user. """
+
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """ Retrieve and return the authenticated user. """
+        return self.request.user
